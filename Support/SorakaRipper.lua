b2b483d3810072805b632401773015f5337a5988
TheRipperGos <zmarvio22@gmail.com>
2017-07-05 00:01:03 -0300

    Update SorakaRipper.lua

--- Support/SorakaRipper.lua
+++ Support/SorakaRipper.lua
@@ -1,7 +1,7 @@
 require 'DamageLib'
 require 'Eternal Prediction'
 require "MapPosition"
-local ScriptVersion = "v0.8"
+local ScriptVersion = "v0.9a"
 -- engine --
 local function Ready(spell)
 	return myHero:GetSpellData(spell).currentCd == 0 and myHero:GetSpellData(spell).level > 0 and myHero:GetSpellData(spell).mana <= myHero.mana and Game.CanUseSpell(spell) == 0 
@@ -85,12 +85,20 @@ function IsChannelling(unit)
 	return result
 end
 
-local function GetDistance(p1,p2)
-return  math.sqrt(math.pow((p2.x - p1.x),2) + math.pow((p2.y - p1.y),2) + math.pow((p2.z - p1.z),2))
+local sqrt = math.sqrt
+
+local function GetDistanceSqr(p1, p2)
+    local dx = p1.x - p2.x
+    local dz = p1.z - p2.z
+    return (dx * dx + dz * dz)
+end
+
+local function GetDistance(p1, p2)
+    return sqrt(GetDistanceSqr(p1, p2))
 end
 
 local function GetDistance2D(p1,p2)
-return  math.sqrt(math.pow((p2.x - p1.x),2) + math.pow((p2.y - p1.y),2))
+    return sqrt((p2.x - p1.x)*(p2.x - p1.x) + (p2.y - p1.y)*(p2.y - p1.y))
 end
 
 local _AllyHeroes
@@ -148,6 +156,96 @@ local function GetMode()
 	end
 end
 
+local _OnVision = {}
+function OnVision(unit)
+	if _OnVision[unit.networkID] == nil then _OnVision[unit.networkID] = {state = unit.visible , tick = GetTickCount(), pos = unit.pos} end
+	if _OnVision[unit.networkID].state == true and not unit.visible then _OnVision[unit.networkID].state = false _OnVision[unit.networkID].tick = GetTickCount() end
+	if _OnVision[unit.networkID].state == false and unit.visible then _OnVision[unit.networkID].state = true _OnVision[unit.networkID].tick = GetTickCount() end
+	return _OnVision[unit.networkID]
+end
+
+local visionTick = GetTickCount()
+function OnVisionF()
+	if GetTickCount() - visionTick > 100 then
+		for i,v in pairs(GetEnemyHeroes()) do
+			OnVision(v)
+		end
+	end
+end
+
+local _OnWaypoint = {}
+function OnWaypoint(unit)
+	if _OnWaypoint[unit.networkID] == nil then _OnWaypoint[unit.networkID] = {pos = unit.posTo , speed = unit.ms, time = Game.Timer()} end
+	if _OnWaypoint[unit.networkID].pos ~= unit.posTo then 
+		_OnWaypoint[unit.networkID] = {startPos = unit.pos, pos = unit.posTo , speed = unit.ms, time = Game.Timer()}
+			DelayAction(function()
+				local time = (Game.Timer() - _OnWaypoint[unit.networkID].time)
+				local speed = GetDistance2D(_OnWaypoint[unit.networkID].startPos,unit.pos)/(Game.Timer() - _OnWaypoint[unit.networkID].time)
+				if speed > 1250 and time > 0 and unit.posTo == _OnWaypoint[unit.networkID].pos and GetDistance(unit.pos,_OnWaypoint[unit.networkID].pos) > 200 then
+					_OnWaypoint[unit.networkID].speed = GetDistance2D(_OnWaypoint[unit.networkID].startPos,unit.pos)/(Game.Timer() - _OnWaypoint[unit.networkID].time)
+				end
+			end,0.05)
+	end
+	return _OnWaypoint[unit.networkID]
+end
+
+local function GetPred(unit, speed, delay)
+	local speed = speed or math.huge
+	local delay = delay or 0.25
+	local unitSpeed = unit.ms
+	if OnWaypoint(unit).speed > unitSpeed then unitSpeed = OnWaypoint(unit).speed end
+	if OnVision(unit).state == false then
+		local unitPos = unit.pos + Vector(unit.pos,unit.posTo):Normalized() * ((GetTickCount() - OnVision(unit).tick)/1000 * unitSpeed)
+		local predPos = unitPos + Vector(unit.pos,unit.posTo):Normalized() * (unitSpeed * (delay + (GetDistance(myHero.pos,unitPos)/speed)))
+		if GetDistance(unit.pos,predPos) > GetDistance(unit.pos,unit.posTo) then predPos = unit.posTo end
+		return predPos
+	else
+		if unitSpeed > unit.ms then
+			local predPos = unit.pos + Vector(OnWaypoint(unit).startPos,unit.posTo):Normalized() * (unitSpeed * (delay + (GetDistance(myHero.pos,unit.pos)/speed)))
+			if GetDistance(unit.pos,predPos) > GetDistance(unit.pos,unit.posTo) then predPos = unit.posTo end
+			return predPos
+		elseif HasBuff(unit,"recall" or "zhonyasringshield" or "willrevive",5 or 11 or 24 or 29 or 31 --[[cc]]) then
+			return unit.pos
+		else
+			return unit:GetPrediction(speed,delay)
+		end
+	end
+end
+
+local castSpell = {state = 0, tick = GetTickCount(), casting = GetTickCount() - 1000, mouse = mousePos}
+
+local function CustomCast(spell, pos, delay)
+	if pos == nil then return end
+	if _G.EOWLoaded or _G.SDK then
+		Control.CastSpell(spell, pos)
+	elseif _G.GOS then
+		local ticker = GetTickCount()
+		if castSpell.state == 0 and ticker - castSpell.casting > delay + Game.Latency() and pos:ToScreen().onScreen then
+			castSpell.state = 1
+			castSpell.mouse = mousePos
+			castSpell.tick = ticker
+		end
+		if castSpell.state == 1 then
+			if ticker - castSpell.tick < Game.Latency() then
+				Control.SetCursorPos(pos)
+				Control.KeyDown(spell)
+				Control.KeyUp(spell)
+				castSpell.casting = ticker + delay
+				DelayAction(function()
+					if castSpell.state == 1 then
+						Control.SetCursorPos(castSpell.mouse)
+						castSpell.state = 0
+					end
+				end, Game.Latency()/1000)
+			end
+			if ticker - castSpell.casting > Game.Latency() then
+				Control.SetCursorPos(castSpell.mouse)
+				castSpell.state = 0
+			end
+		end
+	end
+end
+
 local function EnableOrb(bool)
 	if _G.EOWLoaded then
 		EOW:SetMovements(bool)
@@ -161,7 +259,7 @@ local function EnableOrb(bool)
 	end
 end
 
-local function CastSpell(hotkey,slot,target,predmode)
+--[[local function CastSpell(hotkey,slot,target,predmode)
 	local data = { range = myHero:GetSpellData(slot).range, delay = myHero:GetSpellData(slot).delay, speed = myHero:GetSpellData(slot).speed, width = myHero:GetSpellData(slot).width}
 	local spell = Prediction:SetSpell(data, predmode, true)
 	local pred = spell:GetPrediction(target,myHero.pos)
@@ -179,7 +277,7 @@ local function CastMinimap(hotkey,slot,target,predmode)
 	if pred and pred.hitChance >= 0.25 then
 		Control.CastSpell(hotkey, pred.castPos:ToMM().x,pred.castPos:ToMM().y)
 	end
-end
+end]]
 -- engine --
 -- Soraka -- 
 class "Soraka"
@@ -265,10 +363,6 @@ function Soraka:LoadMenu()
   	TRS.Drawings:MenuElement({id = "E", name = "Draw [E] range", value = true, leftIcon = E.icon})
   	TRS.Drawings:MenuElement({id = "Width", name = "Width", value = 2, min = 1, max = 5, step = 1})
 	TRS.Drawings:MenuElement({id = "Color", name = "Color", color = Draw.Color(255, 0, 0, 255)})
-	--------- Prediction --------------------------------------------------------------------
-	TRS:MenuElement({type = MENU, id = "Pred", name = "Prediction Settings"})
-	TRS.Pred:MenuElement({id = "QChance", name = "[Q] Hitchance", value = 0.125, min = 0.0, max = 1, step = 0.025})
-	TRS.Pred:MenuElement({id = "EChance", name = "[E] Hitchance", value = 0.125, min = 0.0, max = 1, step = 0.025})
 end
 
 function Soraka:Tick()
@@ -289,25 +383,13 @@ function Soraka:Tick()
 end
 
 function Soraka:CastQ(target)
-    local data = { range = myHero:GetSpellData(_Q).range, delay = myHero:GetSpellData(_Q).delay, speed = myHero:GetSpellData(_Q).speed, width = myHero:GetSpellData(_Q).width}
-    local spell = Prediction:SetSpell(data, TYPE_CIRCULAR, false)
-    local pred = spell:GetPrediction(target,myHero.pos)
-    if pred and pred.hitChance >= TRS.Pred.QChance:Value() then
-	EnableOrb(false)
-	Control.CastSpell(HK_Q, pred.castPos)
-	EnableOrb(true)
-    end
+            local pos = GetPred(target, Q.Speed, 0.25 + (Game.Latency()/1000))
+			CustomCast(HK_Q, pos, 250)
 end
 
 function Soraka:CastE(target)
-    local data = { range = myHero:GetSpellData(_E).range, delay = myHero:GetSpellData(_E).delay, speed = myHero:GetSpellData(_E).speed, width = myHero:GetSpellData(_E).width}
-    local spell = Prediction:SetSpell(data, TYPE_CIRCULAR, false)
-    local pred = spell:GetPrediction(target,myHero.pos)
-    if pred and pred.hitChance >= TRS.Pred.EChance:Value() then
-	EnableOrb(false)
-	Control.CastSpell(HK_E, pred.castPos)
-	EnableOrb(true)
-    end
+            local pos = GetPred(target, E.Speed, 0.25 + (Game.Latency()/1000))
+			CustomCast(HK_E, pos, 250)
 end
 	
 function Soraka:Combo()
@@ -326,7 +408,7 @@ function Soraka:Clear()
 	for i = 1, Game.MinionCount() do
 	local minion = Game.Minion(i)
       	if myHero.pos:DistanceTo(minion.pos) < 800 and Ready(_Q) and myHero.mana/myHero.maxMana > TRS.Clear.Mana:Value() then
-		if MinionsAround(minion.pos,235,200) >= TRS.Clear.HQ:Value() and Ready(_Q) and minion.team == 200 or 300 then
+		if MinionsAround(minion.pos,235,200 or 300) >= TRS.Clear.HQ:Value() and Ready(_Q) then
 			Control.CastSpell(HK_Q,minion.pos)
 			return
 		end
@@ -338,10 +420,10 @@ function Soraka:Harass()
 	local target = GetTarget(925)
 	if not target then return end
     if myHero.pos:DistanceTo(target.pos) < 800 and (myHero.mana/myHero.maxMana > TRS.Harass.Mana:Value() / 100) and TRS.Harass.Q:Value() and Ready(_Q) then
-        CastSpell(HK_Q,_Q,target,TYPE_CIRCULAR)
+        self.CastQ(target)
     end
     if myHero.pos:DistanceTo(target.pos) < 925 and (myHero.mana/myHero.maxMana > TRS.Harass.Mana:Value() / 100) and TRS.Harass.E:Value() and Ready(_E) then
-        CastSpell(HK_E,_E,target,TYPE_CIRCULAR)
+        self.CastE(target)
     end
 end
 
@@ -349,10 +431,10 @@ function Soraka:Flee()
 	local target = GetTarget(925)
   	if not target then return end
     if myHero.pos:DistanceTo(target.pos) < 800 and TRS.Flee.Q:Value() and Ready(_Q) then
-        CastSpell(HK_Q,_Q,target,TYPE_CIRCULAR)
+        self.CastQ(target)
     end
     if myHero.pos:DistanceTo(target.pos) < 925  and TRS.Flee.E:Value() and Ready(_E) then
-        CastSpell(HK_E,_E,target,TYPE_CIRCULAR)
+        self.CastE(target)
     end
 	for i,ally in pairs(GetAllyHeroes()) do
 		if not ally.isMe then
@@ -404,10 +486,10 @@ function Soraka:Misc()
 	local hero = Game.Hero(i)
 		if hero and hero.isEnemy and myHero.pos:DistanceTo(hero.pos) < 925 then
 			if Ready(_E) and hero.isChanneling--[[IsChannelling(hero)]] and TRS.Misc.CancelE:Value() then
-				  CastSpell(HK_E,_E,hero,TYPE_CIRCULAR)
+				  self.CastE(hero)
 			end
 			if Ready(_E) and HasBuff(hero,"recall" or "zhonyasringshield" or "willrevive",5 or 11 or 24 or 29 or 31 --[[cc]]) and TRS.Misc.CCE:Value() then
-				  CastSpell(HK_E,_E,hero,TYPE_CIRCULAR)
+				  self.CastE(hero)
 			end
 		end
 	end
